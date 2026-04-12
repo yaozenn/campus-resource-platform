02de7de70bcb1a099aa411aceeb20f58f00349c1
Your Name <your.email@example.com>
2026-04-12 23:17:21 +0800

    修复论坛列表评论数量显示

--- forum/serializers.py
+++ forum/serializers.py
@@ -4,10 +4,14 @@ from users.users_serializers import UserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
+    replies = serializers.SerializerMethodField()
 
     class Meta:
         model = ForumPost
-        fields = ['id', 'title', 'author', 'content', 'post_date', 'status', 'visible_to', 'views']
+        fields = ['id', 'title', 'author', 'content', 'post_date', 'status', 'visible_to', 'views', 'replies']
+
+    def get_replies(self, obj):
+        return obj.comments.count()
 
 class PostCreateSerializer(serializers.ModelSerializer):
     class Meta:
